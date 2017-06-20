a8d09fc917e11710b5efaacb8db3262b4f51061f
liuxl <luxurymask@163.com>
2017-06-20 22:35:23 +0800

    .

---
+++ src/poketheoffer/Match.java
@@ -0,0 +1,19 @@
+package poketheoffer;
+
+public class Match {
+	public boolean match(char[] str, char[] pattern) {
+		for(int i = 0, j = 1, k = 0;i < pattern.length;i++,j++){
+			char current = pattern[i];
+			char next = ((j == pattern.length) ? null : pattern[j]);
+			char currentChar = str[k];
+			if(next == '*'){
+				
+			}else if(current == '.' || current == currentChar){
+				k++;
+				continue;
+			}else{
+				return false;
+			}
+		}
+	}
+}
