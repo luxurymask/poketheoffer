56f0e482249a0a0c7d2bf081411c49ff39b07dad
liuxl <luxurymask@163.com>
2017-09-17 20:29:10 +0800

    .

---
+++ src/searchingajob/sina/SinaTest2.java
@@ -0,0 +1,10 @@
+package searchingajob.sina;
+
+/**
+ * 10亿数据中找出不重复的数。
+ */
+public class SinaTest2 {
+	public static void main(String[] args){
+		
+	}
+}
---
+++ src/searchingajob/sina/SinaTest3.java
@@ -0,0 +1,8 @@
+package searchingajob.sina;
+
+/**
+ * 用户输入用户名前n位时，快速查找可能的用户名。（字典树）
+ */
+public class SinaTest3 {
+	
+}
